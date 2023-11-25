c3fad0034fde42267bb88ea489914c964339e296
milosdjurica <milosdjurica13@gmail.com>
2023-11-25 23:42:49 +0100

    added previous code for simple storage

---
+++ src/AddFiveStorage.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MDN
+
+pragma solidity ^0.8.19;
+
+import {SimpleStorage} from "./SimpleStorage.sol";
+
+// INHERITANCE
+contract AddFiveStorage is SimpleStorage {
+    function sayHello() public pure returns (string memory) {
+        return "Hello";
+    }
+
+    function store(uint256 _newNumber) public override {
+        myFavoriteNumber = _newNumber + 5;
+    }
+}
